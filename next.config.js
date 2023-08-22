0e8ae8069cf5fefa7c3b5c8630094576f7cbf49b
DiiKhanh <duykhanh.030803@gmail.com>
2023-08-22 12:46:24 +0700

    fix cv

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   experimental: {
     serverActions: true
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }
+    });
   }
 };
 
