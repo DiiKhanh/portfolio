5ac4ffe5f50876ca7a1f72d8c1d9e28b9c3dc035
DiiKhanh <duykhanh.030803@gmail.com>
2024-08-17 23:53:28 +0700

    fix: view cv

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -56,7 +56,7 @@ export default function Intro() {
         <span className='text-xl'>Welcome to my personal website! 🚀</span><br/>
         <span className='font-bold'>Hello, I'm Duy Khanh.</span> I'm currently in the third year of <span className='font-bold'> Information Systems at the UIT</span>. I enjoy building <span className='italic'>sites & apps</span>. My focus is{' '}<span className='underline'>researched, and learned about <span className='font-bold'>Frontend Development & Backend and Mobile</span></span>.
       </motion.h1>
-      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -71,14 +71,14 @@ export default function Intro() {
         >
           Contact me here<BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
-        <Link href='/cv.pdf' className='group px-7 py-3 rounded-full bg-white flex items-center gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
-          download='cv-pdk.pdf'
-          locale={false}
+        <a
+          href='https://drive.google.com/file/d/1kNkV1zicqP8sqwQQH03HdqQAqYTdMUQl/view?usp=sharing'
+          className='group px-7 py-3 rounded-full bg-white flex items-center gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
           rel="noopener noreferrer"
-          target='_blank'
-        >
-          Download CV<HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
-        </Link>
+          target='_blank'>
+          Download CV
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+        </a>
         <a
           className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
           href='https://www.linkedin.com/in/ph%E1%BA%A1m-duy-kh%C3%A1nh-740465233'
